8d0ba57e3b3ee38ebaa9b1588c6e3a1e9d3031cb
lazizbek97 <laziz.fayziev@mail.ru>
2022-04-04 12:01:56 +0500

    architecture built

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class RouteGenerator {
   Route? routeGenerate(RouteSettings settings) {
     final args = settings.arguments;
+    switch ("") {
+      case :
+        
+        break;
+      default:
+    }
     
   }
 }
